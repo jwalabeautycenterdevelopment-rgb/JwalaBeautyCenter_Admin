eab429753fcb234b0e9fcddb6026368ade61ad0b
Prakash-WebDad <prakash.s@webdads2u.com>
2026-06-26 12:36:42 +0530

    limited 20 product set

--- src/component/Container/ProductsSection/ProductsSection.jsx
+++ src/component/Container/ProductsSection/ProductsSection.jsx
@@ -272,7 +272,8 @@ const ProductsSection = () => {
     const [isExporting, setIsExporting] = useState(false);
     const [isExportingReimport, setIsExportingReimport] = useState(false);
     const [page, setPage] = useState(1);
-    const limit = 10;
+    const limit = 20;
+
 
     const {
         allProducts,
@@ -290,6 +291,9 @@ const ProductsSection = () => {
     const { allSubCategories } = useSelector((state) => state.subcategory);
     const { allBrands } = useSelector((state) => state.brands);
 
+
+    console.log(pagination);
+    
     useEffect(() => {
         dispatch(getSubCategory());
         dispatch(getBrands());
